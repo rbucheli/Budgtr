c4d7cd8a1b180386d3a6f720c9352441ee305637
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-09-26 22:23:29 -0400

    Most of the functionality added

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-// Dependencies 
-const express = require('express');
+// Dependencies
+const express = require("express");
 const app = express();
 const port = 3000;
 // Database
-const budget = require('./models/budget.js');
+const budget = require("./models/budget.js");
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
 
 // Routes//
 // Index Route
-app.get('/budget', (req, res) => {
-  res.render('index.ejs', { allBudget: budget });
+app.get("/budget", (req, res) => {
+  res.render("index.ejs", { allBudget: budget });
 });
-// Still cannot display array on page
-  
-app.listen(3000);
+
+// Create Route
+app.post("/budget/new", (req, res) => {
+  budget.push(req.body);
+  res.redirect("/budget");
+})
+
+//New Route
+app.get("/budget/new", (req, res) => {
+  res.render("new.ejs", {});
+});
 
 //Show Route
-app.get('/budget/:index', (req, res) => {
-  res.render('show.ejs', {
-    budget: budget[req.params.index]
+app.get("/budget/:index", (req, res) => {
+  res.render("show.ejs", {
+    budget: budget[req.params.index],
   });
 });
 
-//New Route
-
-// Create Route  
+app.listen(3000);
